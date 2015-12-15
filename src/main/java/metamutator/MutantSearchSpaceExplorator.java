e5c2a9e9ac5507276466faee03eb706cc33fa84c
abdelrhamane <abdelrhamane@hotmail.fr>
2015-12-16 00:38:14 +0100

    refactor(MutantSearchSpaceExplorator): delete 100 mutants generated limitation

--- src/main/java/metamutator/MutantSearchSpaceExplorator.java
+++ src/main/java/metamutator/MutantSearchSpaceExplorator.java
@@ -92,8 +92,6 @@ public class MutantSearchSpaceExplorator {
 			System.out.println(selectors.get(sel).getOptionCount());
 			for (int k = 0; k < selectors.get(sel).getOptionCount(); k++) 
 			{
-
-				if (nattempts++>99) break;
 				int[] options = new int[selectors.size()];
 				// System.out.println(Arrays.toString(options));
 				for (int i = options.length - 1; i >= 0; i--) {
